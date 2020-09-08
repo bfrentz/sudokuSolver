02a570cf975a023b58dc15c2dce1587007253d27
bfrentz <bryce.frentz@gmail.com>
2020-09-07 20:18:51 -0400

    Added functions from solver directly into game_gui so that it works more smoothly. Also updated readme

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ Future update ideas:
 * ~~Create a gui which allows the user to play interactively (I was thinking that a terminal version would be good, but it just wasn't fun to look at the board)~~
 * Within game gui, allow for immediate solve / show the solution more cleanly
 * ~~Connect to some api to get starting sudoku boards~~
+* Fix the gui program to get a new board that isn't predefined.
+* Create gui option to select difficulty, new game, end screen,  etc.
 * Allow user to input sudoku from file or clipboard
--- game_gui.py
+++ game_gui.py
@@ -8,16 +8,148 @@ September 2020
 
 import pygame
 import time
+import requests
+import json
 
-from solver import solve, valid_number, get_board
 
 pygame.font.init()
 
 
+"""
+The basic idea in the backtracking algorithm is to just to test each new solution 
+against the constraint and revert to the previous state or step immediately 
+if our current solution isn't valid.
+
+Starting with an incomplete board:
+1. Find an empty space by iterating like reading a book, L->R, Top->Bot
+2. Attempt to place the digits 1-9 in that space
+3. Check if that digit is valid in the current spot based on the current board
+    a. If the digit is valid, recursively attempt to fill the next space (and eventually the
+        whole board) using steps 1-3.
+    b. If it is not valid, reset the square you just filled and go back to the previous step.
+4. Once the board is full by the definition of this algorithm we have found a solution.
+"""
+
+
+def get_board():
+    valid_difficulty = False
+    while not valid_difficulty:
+        diff = input("What difficulty of sudoku puzzle (1=easy, 2=medium, 3=hard)? \n")
+        if diff == '1':
+            difficulty = 'easy'
+            valid_difficulty = True
+        elif diff == '2':
+            difficulty = 'medium'
+            valid_difficulty = True
+        elif diff == '3':
+            difficulty = 'hard'
+            valid_difficulty = True
+        else:
+            print("\nPlease input a valid difficulty.\n")
+
+    link = 'https://sugoku.herokuapp.com/board?difficulty='+difficulty
+    req = requests.get(link)
+    board = json.loads(req.text[9:-2])
+    return board
+
+    # #DEBUG
+    # print(req.text[9:-2])
+    # print(b[2])
+
+# DEBUG
+#get_board()
+
+
+def find_empty(board):
+    """
+    Finds the next empty space in the board
+    input: board: 2d list of ints and empty characters (could be 'X', 0, or whatever)
+    output: tuple (int, int) for row col of next empty spot
+    """
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)    # (row, column)
+
+    # If there are no more empty spots don't return
+    return None
+
+# DEBUG
+#print(find_empty(board1))
+
+
+def valid_number(board, number, position):
+    """
+    Returns if the attempted move is valid
+    input: board: 2d list of ints and empty character (could be 'X', 0, or whatever)
+    input: number: int of guess
+    input: position: tuple for spot in grid (row, column)
+    output: bool
+    """
+
+
+    # Check row
+    for i in range(len(board[position[0]])):
+        #print(board[position[0]][i], end=' ')
+        if board[position[0]][i] == number and position[1] != i:
+            #print("Number in row.")
+            return False
+
+    #print()
+
+    # Check column
+    for j in range(len(board)):
+        #print(board[j][position[1]], end='\n')
+        if board[j][position[1]] == number and position[0] != j:
+            #print("Number in col.")
+            return False
+
+    # Check boxes
+    # 0,0 | 0,1 | 0,2
+    # ---------------
+    # 1,0 | 1,1 | 1,2
+    # ---------------
+    # 2,0 | 2,1 | 2,2
+
+    box_x = position[0] // 3
+    box_y = position[1] // 3
+
+    # # DEBUG
+    # print()
+    # print(box_x, box_y)
+    # print()
+
+    for i in range(box_x*3, box_x*3+3):
+        for j in range(box_y*3, box_y*3+3):
+            # # DEBUG
+            # print(i, j)
+            # print(board[i][j])
+            if board[i][j] == number and position[0] != i and position[1] !=j:
+                return False
+
+    return True
+
+
+# DEBUG
+#print(valid_number(board1, 4, (6,0)))
+
+
+
 # Grid is...
 class Grid:
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
 
-    board = get_board()
+    #board = get_board()
 
     def __init__(self, rows, columns, width, height):
         self.rows = rows
@@ -26,6 +158,7 @@ class Grid:
         self.height = height
         self.model = None
         self.selected = None
+        #self.board = get_board()
         self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(columns)] for i in range(rows)]
 
     def update_model(self):
@@ -37,7 +170,7 @@ class Grid:
             self.cubes[row][column].set(val)
             self.update_model()
 
-            if valid_number(self.model, val, (row,column)) and solve(self.model):
+            if valid_number(self.model, val, (row,column)) and self.solve():
                 return True
             else:
                 self.cubes[row][column].set(0)
@@ -108,6 +241,32 @@ class Grid:
 
         return True
 
+
+    def solve(self):
+        """
+        Solves a sudoku board recursively using backtracking
+        input: board: 2d list of ints and empty character (could be 'X', 0, or whatever)
+        output: solved board
+        """
+        # Check and return for last square
+        empty = find_empty(self.model)
+        if not empty:
+            return True
+        else:
+            row, column = empty
+
+        # Loop through numbers 1-9 to see if they work
+        for i in range(1, 10):
+            if valid_number(self.model, i, (row, column)):
+                self.model[row][column] = i
+
+                # Recursive check
+                if self.solve():
+                    return True
+
+                self.model[row][column] = 0
+
+        return False
        
 
 
