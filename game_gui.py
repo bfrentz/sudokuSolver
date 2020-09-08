a20199012051d23046c7bf775aa38a84db85de98
bfrentz <bryce.frentz@gmail.com>
2020-09-07 21:09:24 -0400

    Added more complete comments and rearranged the code a bit. Need to fix the issues with getting a new board next.

--- game_gui.py
+++ game_gui.py
@@ -11,9 +11,7 @@ import time
 import requests
 import json
 
-
 pygame.font.init()
-
 
 """
 The basic idea in the backtracking algorithm is to just to test each new solution 
@@ -30,113 +28,12 @@ Starting with an incomplete board:
 4. Once the board is full by the definition of this algorithm we have found a solution.
 """
 
-
-def get_board():
-    valid_difficulty = False
-    while not valid_difficulty:
-        diff = input("What difficulty of sudoku puzzle (1=easy, 2=medium, 3=hard)? \n")
-        if diff == '1':
-            difficulty = 'easy'
-            valid_difficulty = True
-        elif diff == '2':
-            difficulty = 'medium'
-            valid_difficulty = True
-        elif diff == '3':
-            difficulty = 'hard'
-            valid_difficulty = True
-        else:
-            print("\nPlease input a valid difficulty.\n")
-
-    link = 'https://sugoku.herokuapp.com/board?difficulty='+difficulty
-    req = requests.get(link)
-    board = json.loads(req.text[9:-2])
-    return board
-
-    # #DEBUG
-    # print(req.text[9:-2])
-    # print(b[2])
-
-# DEBUG
-#get_board()
-
-
-def find_empty(board):
-    """
-    Finds the next empty space in the board
-    input: board: 2d list of ints and empty characters (could be 'X', 0, or whatever)
-    output: tuple (int, int) for row col of next empty spot
-    """
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
-                return (i, j)    # (row, column)
-
-    # If there are no more empty spots don't return
-    return None
-
-# DEBUG
-#print(find_empty(board1))
-
-
-def valid_number(board, number, position):
-    """
-    Returns if the attempted move is valid
-    input: board: 2d list of ints and empty character (could be 'X', 0, or whatever)
-    input: number: int of guess
-    input: position: tuple for spot in grid (row, column)
-    output: bool
-    """
-
-
-    # Check row
-    for i in range(len(board[position[0]])):
-        #print(board[position[0]][i], end=' ')
-        if board[position[0]][i] == number and position[1] != i:
-            #print("Number in row.")
-            return False
-
-    #print()
-
-    # Check column
-    for j in range(len(board)):
-        #print(board[j][position[1]], end='\n')
-        if board[j][position[1]] == number and position[0] != j:
-            #print("Number in col.")
-            return False
-
-    # Check boxes
-    # 0,0 | 0,1 | 0,2
-    # ---------------
-    # 1,0 | 1,1 | 1,2
-    # ---------------
-    # 2,0 | 2,1 | 2,2
-
-    box_x = position[0] // 3
-    box_y = position[1] // 3
-
-    # # DEBUG
-    # print()
-    # print(box_x, box_y)
-    # print()
-
-    for i in range(box_x*3, box_x*3+3):
-        for j in range(box_y*3, box_y*3+3):
-            # # DEBUG
-            # print(i, j)
-            # print(board[i][j])
-            if board[i][j] == number and position[0] != i and position[1] !=j:
-                return False
-
-    return True
-
-
-# DEBUG
-#print(valid_number(board1, 4, (6,0)))
-
-
-
-# Grid is...
 class Grid:
+    """
+    The grid is the board object basically. It contains the values and the model for the board.
+    The grid is a collection of the cube objects
+    Functions allow for modification of the board and the values within.
+    """
     board = [
         [7, 8, 0, 4, 0, 0, 1, 2, 0],
         [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -162,9 +59,23 @@ class Grid:
         self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(columns)] for i in range(rows)]
 
     def update_model(self):
+        """
+        The model is a 'behind the scenes board'
+        Used for when the program sends the board to solve to see. 
+        Needs to be updated for each state and doesn't care about sketch values.
+        Just resets the model board to the actual board values
+        """
         self.model = [[self.cubes[i][j].value for j in range(self.columns)] for i in range(self.rows)]
 
     def place(self, val):
+        """
+        Determines the ability to place a value in this square.
+        First, checks if the value is valid for the square and then calls the 
+        solve method to see if we can come to solution
+        from this decision. 
+        input: val: the int value the user is putting in the square.
+        output: bool for whether or not the move is correct.
+        """
         row, column = self.selected
         if self.cubes[row][column].value == 0:
             self.cubes[row][column].set(val)
@@ -179,11 +90,19 @@ class Grid:
                 return False
 
     def sketch(self, val):
+        """
+        Places the temporary value
+        input: int val is the temporary value in the spot before confirmation
+        """
         row, column = self.selected
         self.cubes[row][column].set_temp(val)
 
 
     def draw(self, win):
+        """
+        Draws the grid window, cubes, and lines
+        input: pygame window
+        """
         # Draw grid lines
         gap = self.width / 9
         for i in range(self.rows+1):
@@ -202,6 +121,10 @@ class Grid:
 
 
     def select(self, row, column):
+        """
+        Chooses the active square and updates the selected position
+        input: int row, column are the location of the current cell
+        """
         # Reset others
         for i in range(self.rows):
             for j in range(self.columns):
@@ -212,6 +135,9 @@ class Grid:
 
 
     def clear(self):
+        """
+        Clears the selected cell and sets to 0
+        """
         row, column = self.selected
         if self.cubes[row][column].value == 0:
             self.cubes[row][column].set_temp(0)
@@ -219,7 +145,7 @@ class Grid:
 
     def click(self, position):
         """
-        input: position is the tuple of coordinate
+        input: position is the int tuple of coordinate
         output: int tuple (row, column)
         """
 
@@ -233,6 +159,9 @@ class Grid:
 
 
     def is_finished(self):
+        """
+        output: bool for if there are any empty squares
+        """
         # Check for any empty spots
         for i in range(self.rows):
             for j in range(self.columns):
@@ -269,11 +198,10 @@ class Grid:
         return False
        
 
-
-# Cubes are....
 class Cube:
-    standard = 9
-
+    """
+    Cubes are the individual squares within the board. They store/print their values and use their size info for drawing
+    """
     def __init__(self, value, row, column, width, height):
         self.value = value
         self.temp = 0
@@ -284,31 +212,139 @@ class Cube:
         self.selected = False
 
     def draw(self, win):
+        """
+        Draws the individual squares within the grid
+        input: win is the pygame window for the board
+        """
         fnt = pygame.font.SysFont("comicsans", 40)
 
         gap = self.width / 9
         x = self.column * gap
         y = self.row * gap
 
+        # Sketch the temp value
         if self.temp != 0 and self.value == 0:
             text = fnt.render(str(self.temp), 1, (128,128,128))
             win.blit(text, (x+5, y+5))
+        
+        # Draw the actual value
         elif not (self.value == 0):
             text = fnt.render(str(self.value), 1, (0,0,0))
             win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
 
+        # Red rectangle around active square
         if self.selected:
             pygame.draw.rect(win, (255,0,0), (x,y,gap,gap), 3)
 
 
     def set(self, val):
+        """
+        Change value
+        input: int val is the value
+        """
         self.value = val
 
     def set_temp(self, val):
+        """
+        Change temporary value
+        input: int val
+        """
         self.temp = val
 
 
+
+def get_board():
+    """
+    Gets a new board as a list of int lists from sugoku.com
+    output: 2d list of int lists for the starting board
+    """
+    valid_difficulty = False
+    while not valid_difficulty:
+        diff = input("What difficulty of sudoku puzzle (1=easy, 2=medium, 3=hard)? \n")
+        if diff == '1':
+            difficulty = 'easy'
+            valid_difficulty = True
+        elif diff == '2':
+            difficulty = 'medium'
+            valid_difficulty = True
+        elif diff == '3':
+            difficulty = 'hard'
+            valid_difficulty = True
+        else:
+            print("\nPlease input a valid difficulty.\n")
+
+    link = 'https://sugoku.herokuapp.com/board?difficulty='+difficulty
+    req = requests.get(link)
+    board = json.loads(req.text[9:-2])
+    return board
+
+
+def find_empty(board):
+    """
+    Finds the next empty space in the board
+    input: board: 2d list of ints and empty characters (could be 'X', 0, or whatever)
+    output: tuple (int, int) for row col of next empty spot
+    """
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)    # (row, column)
+
+    # If there are no more empty spots don't return
+    return None
+
+
+def valid_number(board, number, position):
+    """
+    Returns if the attempted move is valid
+    input: board: 2d list of ints and empty character (could be 'X', 0, or whatever)
+    input: number: int of guess
+    input: position: tuple for spot in grid (row, column)
+    output: bool
+    """
+    # Check row
+    for i in range(len(board[position[0]])):
+        #print(board[position[0]][i], end=' ')
+        if board[position[0]][i] == number and position[1] != i:
+            #print("Number in row.")
+            return False
+
+    # Check column
+    for j in range(len(board)):
+        #print(board[j][position[1]], end='\n')
+        if board[j][position[1]] == number and position[0] != j:
+            #print("Number in col.")
+            return False
+
+    # Check boxes
+    # 0,0 | 0,1 | 0,2
+    # ---------------
+    # 1,0 | 1,1 | 1,2
+    # ---------------
+    # 2,0 | 2,1 | 2,2
+
+    box_x = position[0] // 3
+    box_y = position[1] // 3
+
+    for i in range(box_x*3, box_x*3+3):
+        for j in range(box_y*3, box_y*3+3):
+            # # DEBUG
+            # print(i, j)
+            # print(board[i][j])
+            if board[i][j] == number and position[0] != i and position[1] !=j:
+                return False
+
+    return True
+
+
 def redraw_window(win, board, time, strikes):
+    """
+    Updates the whole board in the game window
+    input: win is pygame window for board
+    input: board is the grid object
+    input: total playtime from system
+    input: strikes are the number of wrong clicks
+    """
     win.fill((255,255,255))
 
     # Draw time
@@ -324,33 +360,51 @@ def redraw_window(win, board, time, strikes):
     board.draw(win)
 
 
-def format_time(seconds):
-    sec = seconds%60
-    minute = sec//60
-    hour = minute//60
+def format_time(time):
+    """
+    Changes time display to HH:MM:SS
+    input: time is the elapsed time 
+    """
+    seconds = time%60
+    minutes = seconds//60
+    hours = minutes//60
 
-    elapsed = ' ' + str(minute) + ':' + str(sec)
+    if hours == 0:
+        elapsed = ' ' + str(minutes) + ':' + str(seconds)
+    else:
+        elapsed = ' ' + str(hours) + ':' + str(minutes) + ':' + str(secs)
+
     return elapsed
 
 
 # Main game
 def main():
-    title = 'Sudoku Game!'
 
-    win = pygame.display.set_mode((540, 600))
+    title = 'Sudoku Game!'
+    window_width = 540
+    window_height = 600
+    grid_size = 9
+
+    # Initialize pygame window
+    win = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption(title)
 
-    board = Grid(9, 9, 540, 540)
+    # Initialize board with the same window width but shorter height for base
+    board = Grid(grid_size, grid_size, window_width, window_width)
 
     key = None
     run = True
     start = time.time()
     strikes = 0
 
+
+    # Gameplay
     while run:
 
+        # Elapsed time
         play_time = round(time.time() - start)
 
+        # Event actions
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -373,24 +427,25 @@ def main():
                     key = 8
                 if event.key == pygame.K_9:
                     key = 9
-                if event.key == pygame.K_DELETE:
+                if event.key == pygame.K_BACKSPACE:
                     board.clear()
                     key = None
                 if event.key == pygame.K_RETURN:
                     i, j = board.selected
                     if board.cubes[i][j].temp != 0:
                         if board.place(board.cubes[i][j].temp):
-                            print('Success!')
+                            #print('Correct number!')
                         else:
-                            print('Incorrect.')
+                            print(str(key) + ' is incorrect.')
                             strikes += 1
                         key = None
 
+                        # End game
                         if board.is_finished():
                             print('The game has ended.')
                             run = False
 
-
+            # Select square
             if event.type == pygame.MOUSEBUTTONDOWN:
                 position = pygame.mouse.get_pos()
                 clicked = board.click(position)
@@ -398,9 +453,11 @@ def main():
                     board.select(clicked[0], clicked[1])
                     key = None
 
+        # Draw temp value
         if board.selected and key != None:
             board.sketch(key)
 
+        # Update window
         redraw_window(win, board, play_time, strikes)
         pygame.display.update()
 
